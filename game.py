ae54ff8fa9d8d24f09fb22a345b18ee8c7ded475
Adam <adamwmcneil@gmail.com>
2023-05-14 14:12:51 -0500

    test

--- game.py
+++ game.py
@@ -1,13 +1,21 @@
 import pygame, sys
 
-turret = pygame.image.load('turret.png')
-DEFAULT_IMAGE_SIZE = (100, 100)
-turret = pygame.transform.scale(turret, DEFAULT_IMAGE_SIZE)
-turret = pygame.transform.rotate(turret, 90)
-turret = pygame.transform.rotate(turret, 90)
+red = (255, 0, 0)        # red
+green = (0, 255, 0)      # green
+blue = (0, 0, 255)       # blue
+yellow = (255, 255, 0)   # yellow
+magenta = (255, 0, 255)  # magenta
+cyan = (0, 255, 255)     # cyan
+white = (255, 255, 255)  # white
+black = (0, 0, 0)        # black
 
-w=600
-h=600
+turret = pygame.image.load('turret.png')
+fireball = pygame.image.load('fireball.png')
+DEFAULT_IMAGE_SIZE = (200, 200)
+turret = pygame.transform.scale(turret, DEFAULT_IMAGE_SIZE)
+fireball = pygame.transform.scale(fireball, DEFAULT_IMAGE_SIZE)
+w=1200
+h=1200
 x = w/2-turret.get_width()/2
 y=h/2-turret.get_height()/2
 
@@ -16,7 +24,16 @@ pygame.init()
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("hello ben")
 
+color = (255, 255, 255)
+screen.fill(color)
+
+for i in range(0, w, 200):
+   pygame.draw.line(screen, black, (i,0), (i,h))
+   pygame.draw.line(screen, black, (0,i), (w,i))
+   print (i)
+
 screen.blit(turret, (x,y))
+screen.blit(fireball, (x,y))
 pygame.display.update()
 
 
@@ -25,3 +42,10 @@ while True:
       if event.type == pygame.QUIT:
          pygame.quit()
          sys.exit()
+
+      if event.type == pygame.KEYDOWN: 
+         if event.key == pygame.K_a:
+            turret = pygame.transform.rotate(turret, 90)
+            screen.fill(color)
+            screen.blit(turret, (x,y))
+            pygame.display.update()
