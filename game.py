3a9395e0ffe10a3b0549a192d2dc826e3b699e8b
Adam <adamwmcneil@gmail.com>
2023-05-19 22:19:48 -0500

    added draw grid and draw on grid and knight

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame, sys
 
+num_of_square=7
 red = (255, 0, 0)        # red
 green = (0, 255, 0)      # green
 blue = (0, 0, 255)       # blue
@@ -9,31 +10,49 @@ cyan = (0, 255, 255)     # cyan
 white = (255, 255, 255)  # white
 black = (0, 0, 0)        # black
 
-turret = pygame.image.load('turret.png')
-fireball = pygame.image.load('fireball.png')
-DEFAULT_IMAGE_SIZE = (200, 200)
-turret = pygame.transform.scale(turret, DEFAULT_IMAGE_SIZE)
-fireball = pygame.transform.scale(fireball, DEFAULT_IMAGE_SIZE)
 w=1200
 h=1200
-x = w/2-turret.get_width()/2
-y=h/2-turret.get_height()/2
+length_of_square = w//num_of_square
 
+turret = pygame.image.load('turret.png')
+fireball = pygame.image.load('fireball.png')
+knight = pygame.image.load('knight.png')
+DEFAULT_IMAGE_SIZE = (length_of_square, length_of_square)
+turret = pygame.transform.scale(turret, DEFAULT_IMAGE_SIZE)
+fireball = pygame.transform.scale(fireball, DEFAULT_IMAGE_SIZE)
+knight = pygame.transform.scale(knight, DEFAULT_IMAGE_SIZE)
+
+x = 3
+y = 3
 
 pygame.init()
 screen = pygame.display.set_mode((w, h))
-pygame.display.set_caption("hello ben")
+pygame.display.set_caption("turret game")
 
-color = (255, 255, 255)
-screen.fill(color)
 
-for i in range(0, w, 200):
-   pygame.draw.line(screen, black, (i,0), (i,h))
-   pygame.draw.line(screen, black, (0,i), (w,i))
-   print (i)
+screen.fill(white)
 
-screen.blit(turret, (x,y))
-screen.blit(fireball, (x,y))
+
+def draw_grid():
+   for i in range(0, w, length_of_square):
+      pygame.draw.line(screen, black, (i,0), (i,h))
+      pygame.draw.line(screen, black, (0,i), (w,i))
+
+def draw_on_grid(x, y, image):
+   x = length_of_square * x
+   y = length_of_square * y   
+   screen.blit(image, (x,y))
+   pygame.display.update()
+   
+
+
+for i in range(num_of_square):
+   for j in range(num_of_square):
+      draw_on_grid(i, j, knight)
+draw_grid()
+draw_on_grid(1, 2, fireball)
+draw_on_grid(5, 5, turret)
+
 pygame.display.update()
 
 
@@ -46,6 +65,7 @@ while True:
       if event.type == pygame.KEYDOWN: 
          if event.key == pygame.K_a:
             turret = pygame.transform.rotate(turret, 90)
-            screen.fill(color)
+            screen.fill(white)
             screen.blit(turret, (x,y))
+            draw_grid()
             pygame.display.update()
