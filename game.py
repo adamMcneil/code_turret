72dd1ebdf18873a0793a7b042440733600134f54
Adam <adamwmcneil@gmail.com>
2023-05-21 21:50:06 -0500

    added player movement

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 import pygame, sys
 
-num_of_square=7
 red = (255, 0, 0)        # red
 green = (0, 255, 0)      # green
 blue = (0, 0, 255)       # blue
@@ -12,26 +11,22 @@ black = (0, 0, 0)        # black
 
 w=1200
 h=1200
+num_of_square=7
 length_of_square = w//num_of_square
+player_x = 3
+player_y = 3
 
+DEFAULT_IMAGE_SIZE = (length_of_square, length_of_square)
 arrow = pygame.image.load('arrow.png')
 cpu = pygame.image.load('cpu.png')
 virus = pygame.image.load('virus.png')
-DEFAULT_IMAGE_SIZE = (length_of_square, length_of_square)
 arrow = pygame.transform.scale(arrow, DEFAULT_IMAGE_SIZE)
 cpu = pygame.transform.scale(cpu, DEFAULT_IMAGE_SIZE)
 virus = pygame.transform.scale(virus, DEFAULT_IMAGE_SIZE)
 
-x = 3
-y = 3
-
 pygame.init()
 screen = pygame.display.set_mode((w, h))
-pygame.display.set_caption("arrow game")
-
-
-screen.fill(black)
-
+pygame.display.set_caption("HACK")
 
 def draw_grid():
    for i in range(0, w, length_of_square):
@@ -42,19 +37,17 @@ def draw_on_grid(x, y, image):
    x = length_of_square * x
    y = length_of_square * y   
    screen.blit(image, (x,y))
+
+def draw_player():
+   draw_on_grid(player_x,player_y,arrow)
+
+def update_screen():
+   screen.fill(black)
+   draw_player()
+   draw_grid()
    pygame.display.update()
-   
 
-
-for i in range(num_of_square):
-   for j in range(num_of_square):
-      draw_on_grid(i, j, virus)
-draw_grid()
-draw_on_grid(1, 2, cpu)
-draw_on_grid(5, 5, arrow)
-
-pygame.display.update()
-
+update_screen()
 
 while True:
    for event in pygame.event.get():
@@ -62,10 +55,13 @@ while True:
          pygame.quit()
          sys.exit()
 
-      if event.type == pygame.KEYDOWN: 
+      if event.type == pygame.KEYDOWN:
          if event.key == pygame.K_a:
-            arrow = pygame.transform.rotate(arrow, 90)
-            screen.fill(black)
-            screen.blit(arrow, (x,y))
-            draw_grid()
-            pygame.display.update()
+            player_x = player_x - 1
+         if event.key == pygame.K_d:
+            player_x = player_x + 1
+         if event.key == pygame.K_w:
+            player_y = player_y - 1
+         if event.key == pygame.K_s:
+            player_y = player_y + 1
+         update_screen() 
